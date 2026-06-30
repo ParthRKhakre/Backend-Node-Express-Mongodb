915445b95bd25dec0b22fc851d082e00cee4b3c8
ParthRKhakre <parthrkhakre@gmail.com>
2026-06-30 23:32:00 +0530

    delete js express

--- 3 Backend/server.js
+++
@@ -1,6 +0,0 @@
-// Starting Up the Server
-const app = require('./src/app')
-
-app.listen(3000,()=>{
-    console.log("Server Started!");
-})
--- 3 Backend/src/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); // Imported Express 
-
-const app = express(); // Instance of express is created
-
-
-
-module.exports = app;
